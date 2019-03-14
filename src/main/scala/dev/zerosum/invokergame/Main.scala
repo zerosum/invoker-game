7c519f37c2aca42493623032263ca4df848f1481
zerosum <osmtkhs.acc@gmail.com>
2019-03-15 01:36:15 +0900

    Tornado+EMP combo with Actor

--- src/main/scala/dev/zerosum/invokergame/Main.scala
+++ src/main/scala/dev/zerosum/invokergame/Main.scala
@@ -1,30 +1,41 @@
 package dev.zerosum.invokergame
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.{Done, NotUsed}
+import dev.zerosum.invokergame.actors.Invoker
 
 import scala.concurrent.Future
 
 object Main extends App {
 
   implicit val system: ActorSystem = ActorSystem("InvokerGame")
+  implicit val mat: ActorMaterializer = ActorMaterializer()
+
+  private val invokerProps: Props = Props[Invoker]
+  private val invokerRef: ActorRef = system.actorOf(invokerProps, "invoker")
 
   val source: Source[Char, NotUsed] = Source("qwwrdwrd")
 
-  val flow: Flow[Char, String, NotUsed] = Flow[Char].map {
-    case 'q' => "[Quas]"
-    case 'w' => "[Wex]"
-    case 'e' => "[Exort]"
-    case 'r' => "Invoke!!"
-    case 'd' => "Cast primary"
-    case 'f' => "Cast secondary"
-    case _ => ""
+  val flow: Flow[Char, Unit, NotUsed] = Flow[Char].map {
+    case 'q' =>
+      invokerRef ! Invoker.CastElement(Quas)
+    case 'w' =>
+      invokerRef ! Invoker.CastElement(Wex)
+    case 'e' =>
+      invokerRef ! Invoker.CastElement(Exort)
+    case 'r' =>
+      invokerRef ! Invoker.Invoke
+    case 'd' =>
+      invokerRef ! Invoker.CastPrimarySpell
+    case 'f' =>
+      invokerRef ! Invoker.CastSecondarySpell
   }
 
-  val sink: Sink[String, Future[Done]] = Sink.foreach[String](println)
+  val sink: Sink[Unit, Future[Done]] = Sink.foreach[Unit]{ _ =>
+    Thread.sleep(1000)
+  }
 
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
   (source via flow to sink).run()
 }
